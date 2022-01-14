7f08610d43cbfcd051dc547f7cd36205c9fdcb3e
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-01-14 19:12:52 +0330

    update captchaSolver

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //common.js import ocrBot.js
 // const ocrBot = require("./ocrBot.js");
 
-async function reddenPage() {
+async function solveCaptcha() {
   // document.body.style.backgroundColor = "red";
   let input = document.querySelector(".submit-container input");
   console.log(input);
@@ -10,13 +10,13 @@ async function reddenPage() {
   console.log(base64code);
   let code = await getCode(base64code);
   // await fillCapthaInput(code);
-  if(!code.ParsedResults[0].ParsedText) return false
+  if (!code.ParsedResults[0].ParsedText) return false;
   console.log(code.ParsedResults[0].ParsedText);
-    await fillCapthaInput(input, code.ParsedResults[0].ParsedText);
-    return true
+  await fillCapthaInput(input, code.ParsedResults[0].ParsedText);
+  return true;
 }
 
-async function fillCapthaInput(input,captchaCode) {
+async function fillCapthaInput(input, captchaCode) {
   input.focus();
   //simulate typing in the input
   for (let i = 0; i < captchaCode.length; i++) {
@@ -61,38 +61,83 @@ async function requestCaptcha(captchaBase64) {
 
   return await fetch("https://api.ocr.space/parse/image", requestOptions)
     .then((response) => response.json())
-    .catch((error) => runInterval());
+    .catch((error) => false);
 }
 
-
-
-let isRecaptchaFrame = () => {
-    let img = document.querySelector(".submit-container img");
-    if (img) {
-        document.querySelector('.captcha-img').addEventListener('click', () => {
-            console.log('clicked');
-            reddenPage();
-        });
-        
-        document.querySelector('.captcha-text').addEventListener('click', () => {
-            console.log('clicked');
-            reddenPage();
-        });
-        return true;
-    }
-    return false;
-  };
-
-
-let runInterval = () => {
-    let captchaInterval = setInterval(() => {
-        console.log("checking");
-        if (isRecaptchaFrame()) {
-            clearInterval(captchaInterval)
-            reddenPage();
-        }  
-    }, 500);
+let isCaptchaFrame = () => {
+  let img = document.querySelector(".submit-container img");
+  if (img) {
+    return true;
+  }
+  return false;
 };
-runInterval()
+
+let isTermsCheckBox = () => {
+  let checkbox = document.querySelector("#agreement");
+  if (checkbox) {
+    return true;
+  }
+  return false;
+};
+
+var currentURL = location.href;
+setInterval(function () {
+  if (location.href != currentURL) {
+    console.log("url changed");
+    if (isCaptchaFrame()) {
+      // clearInterval(captchaInterval);
+      solveCaptcha();
+
+        document.querySelector('.captcha-img').addEventListener('click', () => {
+          solveCaptcha();
+        });
+
+        document.querySelector('.captcha-text').addEventListener('click', () => {
+          solveCaptcha();
+        });
+      let captchaImage = document.querySelector(".submit-container img");
+      observer = new MutationObserver((changes) => {
+        changes.forEach(change => {
+          if (change.attributeName.includes('src')) {
+            // console.dir(captchaImage.src);
+            solveCaptcha();
+          }
+        });
+      });
+      observer.observe(captchaImage, { attributes: true });
+    }
+    if (isTermsCheckBox()) {
+      // clearInterval(captchaInterval);
+      document.querySelector('#agreement').click();
+    }
+    currentURL = location.href
+  }
+}, 100);
+
+
+
+// let runInterval = () => {
+//   let captchaInterval = setInterval(() => {
+//     console.log("checking");
+//     if (isCaptchaFrame()) {
+//       clearInterval(captchaInterval);
+//       solveCaptcha();
+
+//       document.querySelector('.captcha-img').addEventListener('click', () => {
+//         solveCaptcha();
+//       });
+
+//       document.querySelector('.captcha-text').addEventListener('click', () => {
+//         solveCaptcha();
+//       });
+//     }
+//     if (isTermsCheckBox()) {
+//       clearInterval(captchaInterval);
+//       document.querySelector('#agreement').click();
+//     }
+//   }, 500);
+// };
+
+// runInterval();
 
 // event listener for change src .submit-container img src
